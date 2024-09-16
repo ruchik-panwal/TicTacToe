2fbe4ee2e9a9e5924f73c9777d33b4a4c6cd5d0b
Ruchik <ruchik.panwal@gmail.com>
2024-09-16 21:13:54 +0530

    Made a function which returns a random value form 0 - 9 which can be used for the computer generated TicTactoe decision

--- javascript.js
+++ javascript.js
@@ -1,9 +1,19 @@
 
 gameBoard = [];
 
+// To reset the Game Board for the next Game
 function gameBoardReset() {
-    for(i=0 ; i<9 ; i++)
+    for (i = 0; i < 9; i++)
         gameBoard.push("");
 }
 
 console.log(gameBoard);
+
+// To get a computer generated opponent decision
+function randomChoice() {
+
+    compChoice = parseInt(Math.random() * 10)
+    if (gameBoard[compChoice] == "")
+        return compChoice
+
+}
